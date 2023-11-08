8b23e4f748ee9d5e1593518f6b090d8b83c0b943
monkeymicro <147698074+monkeymicro@users.noreply.github.com>
2023-11-08 14:50:49 +0800

    Add files via upload

---
+++ main_en_zh_ocrv4_0.py
@@ -0,0 +1,59 @@
+
+import fitz
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from paddleocr import PaddleOCR, draw_ocr
+
+ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=2)  # need to run only once to download and load model into memory
+def ppocr_recognize(img_path):
+    # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+    # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+    result = ocr.ocr(img_path, cls=True)
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            print(line)
+    return result
+
+img_path = './translate_pdf/data2.pdf'
+result = ppocr_recognize(img_path)
+
+def extract_formula_from_pdf(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+
+        # 显示图像
+        plt.imshow(img)
+        plt.axis('off')  # 关闭坐标轴
+        plt.show()
+
+# 显示结果
+
+imgs = []
+with fitz.open(img_path) as pdf:
+    for pg in range(0, pdf.pageCount):
+        page = pdf[pg]
+        mat = fitz.Matrix(2, 2) # 2x缩放操作
+        pm = page.getPixmap(matrix=mat, alpha=False)
+        # if width or height > 2000 pixels, don't enlarge the image
+        if pm.width > 2000 or pm.height > 2000:
+            pm = page.getPixmap(matrix=fitz.Matrix(1, 1), alpha=False)
+
+        img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        imgs.append(img) # 多页pdf组成图片list
+        
+for idx in range(len(result)):
+    res = result[idx]
+    image = imgs[idx]
+    boxes = [line[0] for line in res]
+    txts = [line[1][0] for line in res]
+    scores = [line[1][1] for line in res]
+    im_show = draw_ocr(image, boxes, txts, scores, font_path='doc/fonts/simfang.ttf')
+    im_show = Image.fromarray(im_show)
+    im_show.save('result_page_{}.jpg'.format(idx))
